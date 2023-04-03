560792bee2ff705d1e2d08941d46594260b685eb
fingalropl <kirill20.04.2002@yandex.ru>
2023-04-03 22:44:39 +0300

    init

---
+++ bot/main.py
@@ -0,0 +1,117 @@
+import json
+import os
+
+import vk_api
+from dotenv import load_dotenv
+from vk_api.bot_longpoll import VkBotEventType, VkBotLongPoll
+
+# from Apidogs import prepare_img
+from schedule import get_shedule, api
+from text_command import COMMANDS
+load_dotenv()
+
+VK_TOKEN = os.getenv('VK_TOKEN')
+GROUP_ID = os.getenv('GROUP_ID')
+
+vk_session = vk_api.VkApi(token=VK_TOKEN)
+api = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+
+
+def get_but(text):
+    return {
+                "action": {
+                    "type": "text",
+                    "payload": "{\"button\": \"" + "1" + "\"}",
+                    "label": f"{text}"
+                },
+                "color": "positive"
+            }
+
+
+def get_keyboard(text):
+    return {
+        'one_time' : True,
+        'buttons' : [
+            [get_but(text)]
+        ]
+    }
+
+
+def json_keyboard(text):
+    key = get_keyboard(text)
+    keyboard = json.dumps(key, ensure_ascii = False).encode('utf-8')
+    keyboard = str(keyboard.decode('utf-8'))  
+    return keyboard
+
+
+def send_mes(id, text, img):
+    if img is not True:
+        api.messages.send(chat_id = id, message = text, random_id = 0, )
+
+
+# def send_video(id, text, vid):
+#     api.messages.send(chat_id = id, message = text, random_id = 0, attachment = vid)
+
+
+# weekday - показатель дня недели; '1' - завтра, '0' - сегодня
+# def send_shedule(id, weekday):
+#     shedule, text = get_shedule(weekday)
+#     api.messages.send(chat_id = id, message = text, random_id = 0, attachment = shedule)
+
+
+def send_text(id, text):
+    api.messages.send(chat_id = id, message = text, random_id = 0)
+
+
+def off_button(id, but_text):
+    api.messages.send(chat_id = id, message = 'Нажмите, на кнопку "Вырубить кнопку", чтобы отключить старую кнопку.', keyboard = json_keyboard(but_text), random_id = 0)
+
+
+#здесь будет подготовка строки даныых из объекта
+def prepare_text(weekday):
+        print(weekday.first_lesson[0].name)
+    
+
+def distir_schedule(msg, id):
+    word = msg.split()
+    if 'завтра' in word:
+        weekday = api(0,0,1)
+        prepare_text(weekday)
+    else:
+        pass
+     
+
+def distributor(msg, id):
+        if msg == 'привет':
+            send_text(id=id, text=COMMANDS['send_hi'])
+        elif msg == 'выключить кнопку':
+            off_button(id=id, but_text = 'Вырубить кнопку.')
+        elif msg=='что умеешь?' or msg=='что умеешь.' or msg=='что умеешь':
+            send_text(id=id, text=COMMANDS['send_help'])
+        else:
+            send_text(id=id, text=COMMANDS['send_error'])
+
+
+def main():
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat:
+            msg = event.object['message']['text'].lower()
+            print(event)
+            id = event.chat_id
+            try:
+                name,msg = msg.split(',')
+                name = name.strip(' ')
+                msg = msg.strip(' ')
+                if name == 'бот' and 'расписание' in msg:
+                    distir_schedule(msg,id)
+                elif name == 'бот':
+                    distributor(msg,id)
+
+            except Exception as e:
+                print(e)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ bot/schedule.py
@@ -0,0 +1,81 @@
+import datetime as dt
+from datetime import timedelta
+import requests
+from dataclasses import dataclass
+import json
+
+class Weekday:
+  def __init__( self, dict ):
+      vars(self).update( dict )
+    
+# SHEDULE = {1 : {0 : { 'img' : 'photo-215622029_457239125', 'weekday' : 'понедельник'},
+#                 1 : { 'img' : 'photo-215622029_457239124', 'weekday' : 'вторник'},
+#                 2 : { 'img' : 'photo-215622029_457239126', 'weekday' : 'среда'},
+#                 3 : { 'img' : 'photo-215622029_457239127', 'weekday' : 'четверг'},
+#                 4 : { 'img' : 'photo-215622029_457239120', 'weekday' : 'пятница нечетной недели, кайфуй. Вот расписание на следующий четный понедельник!'},
+#                 5 : { 'img' : 'photo-215622029_457239120', 'weekday' : 'суббота нечетной недели, кайфуй. Вот расписание на следующий четный понедельник!'},
+#                 6 : { 'img' : 'photo-215622029_457239120', 'weekday' : 'воскресение нечетной недели, кайфуй. Вот расписание на следующий четный понедельник!'}},
+#            0 : {0 : { 'img' : 'photo-215622029_457239120', 'weekday' : 'понедельник'},
+#                 1 : { 'img' : 'photo-215622029_457239121', 'weekday' : 'вторник'},
+#                 2 : { 'img' : 'photo-215622029_457239122', 'weekday' : 'среда'},
+#                 3 : { 'img' : 'photo-215622029_457239123', 'weekday' : 'четверг'},
+#                 4 : { 'img' : 'photo-215622029_457239125', 'weekday' : 'пятница четной недели, кайфуй. Вот расписание на следующий нечетный понедельник!'},
+#                 5 : { 'img' : 'photo-215622029_457239125', 'weekday' : 'суббота четной недели, кайфуй. Вот расписание на следующий нечетный понедельник!'},
+#                 6 : { 'img' : 'photo-215622029_457239125', 'weekday' : 'воскресение четной недели, кайфуй. Вот расписание на следующий нечетный понедельник!'}}}
+# PARITY_NAME = {1 : 'нечетн',
+#                0 : 'четн'}
+
+
+def date(tommorow):
+    """определяет четная или нечетная неделя. при нечет возвращает - 1, чет - 0 """
+    now = dt.date.today()
+    weekday = dt.date.today().weekday()
+    if tommorow == 1:
+        now = now + timedelta(days=1)
+        weekday = weekday + 1
+        if weekday > 6:
+            weekday = 0
+    begin = dt.date(2023,2,6)
+    delta = (now - begin).days
+    parity =  (delta // 7) % 2 
+    # print(f'Четность: {parity}, День недели: {weekday}') #Принт для проверки
+    return weekday, parity
+
+
+# def get_shedule(day):
+#     if day == 0:
+#         weekday, parity = date(day)
+#         parity_name = PARITY_NAME[parity]
+#         shedule = SHEDULE[parity][weekday]
+#         shedule_name = shedule['weekday']
+#         if weekday == 4 or weekday == 5 or weekday == 6:
+#             text = (f'{shedule_name}.')
+#         else:
+#             text = f'Сегодня {parity_name}ая неделя. {shedule_name}.'
+#         return shedule['img'], text
+#     elif day == 1:
+#         weekday, parity = date(day)
+#         parity_name = PARITY_NAME[parity]
+
+#         shedule = SHEDULE[parity][weekday]
+#         parity_name = PARITY_NAME[parity]
+#         shedule_name = shedule['weekday']
+#         if weekday == 4 or weekday == 5 or weekday == 6:
+#             text = (f'Завтра {shedule_name}')
+#         else:
+#             text = f'Завтра {shedule_name} {parity_name}ой недели '
+#         return shedule['img'], text
+
+
+
+#aditionally: 0 -tommorow, 1 - понедельник, 2 -вторник и т.д
+дописать метод 
+def api(day, parity, additionally):
+    if additionally == 1: 
+        day, parity = date(1)
+        response = requests.get(f'http://127.0.0.1:8000/api/v1/weekday/{day}/?parity={parity}')
+        # print(response.text)
+        weekday = json.loads(response.text, object_hook=Weekday)
+    if additionally == 1:
+        pass
+    return weekday
